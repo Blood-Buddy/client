e40a36ce593f3e5f2ed7b3caec35f98f2c222265
TanyaArdhia <tanyaardhiap@gmail.com>
2024-01-19 14:47:51 +0700

    feat: setting done

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,7 +1,77 @@
-import { Text } from "react-native";
+import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import { AntDesign, MaterialCommunityIcons } from "@expo/vector-icons";
 
-export default function Settings() {
-    return (
-        <Text>Settings Page</Text>
-    )
+export default function Setting() {
+  return (
+    <ScrollView className="bg-[#F2F2F2]">
+      <SafeAreaView className="mx-5 px-5">
+        <View className="border-b-2 flex-row justify-between items-center border-b-red-700">
+          <Text className="font-bold text-2xl mb-2">Setting</Text>
+        </View>
+
+        {/* edit profile */}
+        <View className="">
+          <View style={styles.card} className="flex flex-row">
+            <View className="mr-3">
+              <TouchableOpacity>
+                <MaterialCommunityIcons
+                  name="account-outline"
+                  size={24}
+                  color="black"
+                />
+              </TouchableOpacity>
+            </View>
+            <View className="">
+              <TouchableOpacity>
+                <Text className="text-lg font-medium"> Edit Profile</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+
+        {/* Sign Out */}
+        <View className="">
+          <View style={styles.card} className="flex flex-row">
+            <View className="mr-3">
+              <TouchableOpacity>
+                <AntDesign name="logout" size={23} color="black" />
+              </TouchableOpacity>
+            </View>
+            <View className="">
+              <TouchableOpacity>
+                <Text className="text-lg font-medium"> Sign Out</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </SafeAreaView>
+    </ScrollView>
+  );
 }
+
+const styles = StyleSheet.create({
+  borderBottom: {
+    borderBottomColor: "red",
+    borderBottomWidth: 1,
+  },
+  card: {
+    backgroundColor: "#F2F2F2",
+    // backgroundColor: "yellow",
+    borderRadius: 3,
+    marginTop: 20,
+    padding: 12,
+    // shadowColor: "#000",
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.2,
+    shadowRadius: 1,
+  },
+});
