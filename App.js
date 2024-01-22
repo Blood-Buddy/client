fcd20bbb5a286474c29ff72a75f151d0ea29bbc1
Mukhtar Rafi Fauzi <mukhtar.r.f@gmail.com>
2024-01-22 14:29:34 +0700

    feat: login and logout function done

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigator from './src/navigators/StackNavigator';
+import { AuthComponent } from './src/context/LoginContext';
 export default function App() {
   return (
-    <NavigationContainer>
-      <StackNavigator />
-    </NavigationContainer>
+    <AuthComponent>
+      <NavigationContainer>
+        <StackNavigator />
+      </NavigationContainer>
+    </AuthComponent>
   );
 }
--- src/navigators/StackNavigator.js
+++ src/navigators/StackNavigator.js
@@ -4,37 +4,56 @@ import TabNavigator from "./TabNavigator";
 import History from "../pages/History";
 import BookAppointment from "../pages/BookAppointment";
 import DetailRewards from "../pages/DetailRewards";
+import { useContext } from "react";
+import { LoginContext } from "../context/LoginContext";
+import Login from "../pages/login";
+import Register from "../pages/register";
+console.log(process.env)
 
 const Stack = createStackNavigator();
 
 export default function StackNavigator() {
+  const { isLoggedIn } = useContext(LoginContext);
+  const {setIsLoggedIn} = useContext(LoginContext);
+  // setIsLoggedIn(false)
+  // console.log(isLoggedIn, "isLoggedIn");
+
   return (
-    <Stack.Navigator initialRouteName="Profile">
-      <Stack.Screen
-        name="Home"
-        component={TabNavigator}
-        options={{ headerShown: false, title: "Home" }}
-      />
-      <Stack.Screen
-        name="Account Information"
-        component={AccountInformasi}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="Book Appointment"
-        component={BookAppointment}
-        options={{ headerStyle: { backgroundColor: "#F2F2F2", }, headerBackTitleVisible: false }}
-      />
-      <Stack.Screen
-        name="Detail Rewards"
-        component={DetailRewards}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="History"
-        component={History}
-        options={{ headerShown: false }}
-      />
+    <Stack.Navigator>
+      {isLoggedIn ? (
+        <>
+          <Stack.Screen
+            name="Home"
+            component={TabNavigator}
+            options={{ headerShown: false, title: "Home" }}
+          />
+          <Stack.Screen
+            name="Account Information"
+            component={AccountInformasi}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Book Appointment"
+            component={BookAppointment}
+            options={{ headerStyle: { backgroundColor: "#F2F2F2", }, headerBackTitleVisible: false }}
+          />
+          <Stack.Screen
+            name="Detail Rewards"
+            component={DetailRewards}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="History"
+            component={History}
+            options={{ headerShown: false }}
+          />
+        </>
+      ) : (
+        <>
+          <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+          <Stack.Screen name="Register" component={Register} options={{ title: "Register" }} />
+        </>
+      )}
     </Stack.Navigator>
   );
 }
