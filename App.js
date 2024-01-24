32236513fabb492cff81050593a0512a8077a370
Gema Kusuma <gemaakbarkusuma@gmail.com>
2024-01-25 00:10:49 +0700

    feat: request submit hospital

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigator from './src/navigators/StackNavigator';
 import { AuthComponent } from './src/context/LoginContext';
+import Toast from "react-native-toast-message";
 export default function App() {
   return (
     <AuthComponent>
       <NavigationContainer>
         <StackNavigator />
+          <Toast />
       </NavigationContainer>
     </AuthComponent>
   );
